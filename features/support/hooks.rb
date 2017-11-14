8e4d781e7f42a6a9cba044a4105a4e7f8c812717
FernandoCAbreu <33203920+FernandoCAbreu@users.noreply.github.com>
2017-11-14 09:14:54 -0200

    Add files via upload

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,4 +1,13 @@
+#language: pt
+#utf-8
 
-After do
-	page.driver.browser.save_screenshot 'screenshot.png'
+World(Helper)
+After('@Cadastro_Tasks') do |scenario|
+   ## take screenshot
+   scenario_name = scenario.name.gsub(/\s+/, '_').tr('/', '_')
+   if scenario.failed?
+     take_screenshot(scenario_name.downcase!, 'failed')
+   else
+     take_screenshot(scenario_name.downcase!, 'passed')
+   end
 end
